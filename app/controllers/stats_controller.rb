3dd7db99989f37e4d3897a1dccc1cf17a0770424
ashoordoushow <ashoor12323@gmail.com>
2024-09-26 15:06:06 -0500

    Add create action

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,4 +3,14 @@ class StatsController < ApplicationController
         @stats = Stat.all
         render 'stats/index'
     end
+    
+    def create
+        stat = Stat.new(
+            total_points: 50,
+            minutes_played: 30
+        )
+        p stat
+        stat.save
+        render json: {name: 'hello'}
+    end
 end
--- test/controllers/stats_controller_test.rb
+++ test/controllers/stats_controller_test.rb
@@ -8,4 +8,10 @@ class StatsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Stat.count, data.length
   end
+  test "create" do
+    assert_difference "Stat.count", 1 do
+      post "/stats.json", params: { total_points: 20, minutes_played: 25 }
+      assert_response 200
+    end
+  end
 end
